f72e9ce55fecf431c00ca49309121c2e3da33d52
Alex Stolzoff <alexstolznut@hotmail.com>
2019-08-18 02:09:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ function removeFromCart(item) {
 
   for(var i = 0; i < cart.length; i = i + 1){
     let newItem = getCart()[i];
-    if(Object.keys(cart[i]) === item) {
+    if(Object.keys(newItem) === item) {
 
       console.log(newItem);
     }
