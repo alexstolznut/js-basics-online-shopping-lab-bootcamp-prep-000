5333732229d8caccbe3180a3e11e361edacf3d10
Alex Stolzoff <alexstolznut@hotmail.com>
2019-08-18 02:57:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ function removeFromCart(item) {
   //   return console.log('That item is not in your cart.');
   // }
 
-    console.log(array);
+    console.log(array.indexOf(item));
 
   for(var i = 0; i < cart.length; i = i + 1){
     // console.log(cart.indexOf(getCart()[i][item]));
