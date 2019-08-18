b837e59e0f8d5eb8651607fba7a0b1929f2a458f
Alex Stolzoff <alexstolznut@hotmail.com>
2019-08-18 04:02:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -100,10 +100,12 @@ function placeOrder(cardNumber) {
   if(!cardNumber){
     return console.log("Sorry, we don't have a credit card on file for you.");
   }
+  if(cardNumber){
     const total = total();
     cart = [];
-    return console.log(`Your total cost is $${total}, which will be charged to the card ${cardNumber}.`)
-  
+    return console.log(`Your total cost is $${total}, which will be charged to the card ${cardNumber}.`);
+  }
+
   // write your code here
 }
 
