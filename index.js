af47ed432e1c3a77c464272a235cf35b9b66623b
Alex Stolzoff <alexstolznut@hotmail.com>
2019-08-18 01:48:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ function removeFromCart(item) {
   const index = cart.indexOf(item);
   if(index === -1){
     return console.log('That item is not in your cart');
-  } 
+  }
   cart.splice(index);
   return console.log(getCart());
 }
